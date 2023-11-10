430693951f3f20fb269a01e12c8c0d36a343d56d
Romain Miras <romain.miras@etu.univ-grenoble-alpes.fr>
2023-11-10 14:54:29 +0100

    Question 1 Done + Début Question 2

--- project.ml
+++ project.ml
@@ -1,3 +1,13 @@
+(*****************************************************)
+(* Project : Analyse syntaxique et sémantique *)
+(*Louane LESUR
+  Gregory PITITTO
+  Albin HORLAVILLE
+  Romain MIRAS*)
+(*****************************************************)
+
+(* Exercice 1.1.1 Définir une hiérarchie de types OCaml permettant de représenter tous les programmes ad-
+mis par la description ci-dessus. *)
 type var = A | B | C | D 
 
 type exp = Vexp of var | Bexp of bool
@@ -8,12 +18,22 @@ type stmt =
   | Seq of stmt * stmt
   | If of var * stmt * stmt
   | While of var * stmt;;
-  
-let (:=) v e = Assign (v, e)
 
-let skip = Skip
+#use "./Projet-PF-LT/anacomb.ml";;
+(* Exercice 1.1.2 Donner une grammaire décrivant le langage WHILEb- -. *)
+let var_option (c:char) : var option = match c with
+  | 'A' -> Some A
+  | 'B' -> Some B
+  | 'C' -> Some C
+  | 'D' -> Some D
+  | _ -> None;;
 
-let if_ e s1 s2 = If (e, s1, s2)
+let bool_option (c:char) : bool option = match c with
+  | 'T' -> Some true
+  | 'F' -> Some false
+  | _ -> None;;
 
-let while_ e s = While (e, s)
+let rana_V l = terminal_res var_option l;;
+
+let rana_B l = terminal_res bool_option l;;
 
